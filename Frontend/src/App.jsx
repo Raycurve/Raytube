01751ff435d4164b687b14eeb28beb04cf9e8420
Shantanu Kapse <shantanu.sk05@gmail.com>
2024-12-03 23:29:10 +0530

    Add Menu Structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+
+const Container = styled.div`
+
+  display: flex;
+`;
+const Main = styled.div`
+  //main 7 times the width of other flex element i.e. menu :)
+  flex:7;
+`;
+const Wrapper = styled.div``;
+
+function App() {
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>
+          <h1>video cards</h1>
+          <h1>video cards</h1>
+          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          <h1>video cards</h1>
+          <h1>video cards</h1>          
+
+        </Wrapper>
+      </Main>
+    </Container>
+
+  )
+}
+
+export default App;
---
+++ Frontend/src/components/Menu.jsx
@@ -0,0 +1,168 @@
+import React from 'react'
+import styled from 'styled-components'
+import raytube from "../imgs/verticrop-bgrem.png"
+
+import HomeIcon from '@mui/icons-material/Home';
+import ExploreIcon from '@mui/icons-material/Explore';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import HistoryIcon from '@mui/icons-material/History';  
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import MovieCreationIcon from '@mui/icons-material/MovieCreation';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SettingsIcon from '@mui/icons-material/Settings';
+import FlagCircleIcon from '@mui/icons-material/FlagCircle';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import '@fontsource/roboto/500.css';
+const Container = styled.div `
+  flex:1;
+  color:white;
+  height:100vh;
+  background-color:#202020;
+  font-size:10px;
+  font-family:roboto;
+  font-weight:500;
+  position: sticky;
+  top:0;
+`
+const Wrapper = styled.div `
+padding: 10px 16px;
+`
+const Logo  = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  // gap:5px;
+  padding:7px;
+`
+const Hr =styled.hr`
+  margin: 8px 0px;
+  border: 0.5px solid #373737
+`
+
+const Item = styled.div`
+  
+  display:flex;
+  align-items: center;
+  gap: 16px;
+  cursor:pointer;
+  padding-bottom:8px;
+`
+
+const Img = styled.img`
+  height:12vh;
+`
+const Login = styled.div`
+  font-weight: 300;
+  // font-size: small;
+`
+const Button = styled.button`
+  padding:4px 12px;
+  background-color:transparent;
+  border: 0.5px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 2.5px;
+  font-weight: 500;
+  margin-top: 8px;
+  cursor: pointer;
+  display: flex;
+  align-items:center;
+  gap: 5px;
+`
+export default function Menu() {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src={raytube} />
+        </Logo>
+        <Item>
+          <HomeIcon/>
+          Home  
+        </Item>
+        <Item>
+          <ExploreIcon/>
+           Explore
+        </Item>
+        <Item>
+          <SubscriptionsIcon/>
+          Subscriptions 
+        </Item>
+        <Hr/>
+
+        <Item>
+          <VideoLibraryIcon/>
+          Library  
+        </Item>
+        <Item>
+          <HistoryIcon/>
+          History  
+        </Item>
+
+        <Hr/>
+        <Login>
+          Sign in to like videos comment and subscribe
+          <Button><AccountCircleIcon/> SIGN IN</Button>
+        </Login>
+
+        <Hr/>
+
+        <Item>
+          <LibraryMusicIcon/>
+          Music  
+        </Item>
+        <Item>
+          <SportsBasketballIcon/>
+          Sports  
+        </Item>
+        <Item>
+          <SportsEsportsIcon/>
+          Gaming  
+        </Item>
+        <Item>
+          <MovieCreationIcon/>
+          Movies  
+        </Item>
+        <Item>
+          <NewspaperIcon/>
+          News  
+        </Item>
+        <Item>
+          <LiveTvIcon/>
+          Live  
+        </Item>
+
+        <Hr/>
+
+
+        <Item>
+          <SettingsIcon/>
+          Settings  
+        </Item>
+        <Item>
+          <FlagCircleIcon/>
+          Report  
+        </Item>
+        <Item>
+          <HelpOutlineIcon/>
+          Hello & Support  
+        </Item>
+        <Item>
+          <SettingsBrightnessIcon/>
+          Change Mode  
+        </Item>
+
+
+
+
+
+
+      </Wrapper>
+    </Container>
+  )
+}
